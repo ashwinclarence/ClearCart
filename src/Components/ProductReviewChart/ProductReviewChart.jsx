0c034abb530faf81278708c6874fc314a1cbc6af
ASHWIN CLARENCE <meashwinclarence@gmail.com>
2024-01-05 18:19:57 +0530

    viewproducts completed with product review

---
+++ src/Components/ProductReviewChart/ProductReviewChart.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import './ProductReviewChart.css'
+import Chart from "react-apexcharts";
+function ProductReviewChart() {
+    const [state, setState] = useState({
+          
+        series: [67,44, 55, 90, 83],
+        options: {
+          plotOptions: {
+            radialBar: {
+              dataLabels: {
+                value: {
+                  fontSize: '20px',
+                },
+                total: {
+                  show: true,
+                  label: 'Total',
+                  formatter: function () {
+                    return 249
+                  }
+                }
+              }
+            }
+          },
+          labels: [`\u2B50\u2B50\u2B50\u2B50\u2B50`, `\u2B50\u2B50\u2B50\u2B50`, `\u2B50\u2B50\u2B50`, `\u2B50\u2B50`, `\u2B50`],
+        },
+      })
+      
+      return (
+        <div className='product-review-chart-container container-fluid'>
+          <div className="row product-review-chart-row">
+            <h4>Product Reviews</h4>
+            <div className="product-review-chart-other">
+              <Chart
+                options={state.options}
+                series={state.series}
+                type='radialBar'
+                width={560}
+                height={350}
+              />
+            </div>
+            <div className="product-review-chart-mobile">
+              <Chart
+                options={state.options}
+                series={state.series}
+                type='radialBar'
+                width={350}
+                height={350}
+              />
+            </div>
+          </div>
+        </div>
+      )
+    }
+    
+
+export default ProductReviewChart
--- src/Components/ViewPosts/ViewPosts.jsx
+++ src/Components/ViewPosts/ViewPosts.jsx
@@ -1,23 +1,52 @@
-
+import './ViewPosts.css'
+import defaultImg from '../../images/noimg.jpg'
+import UserViewProducts from '../UserViewProducts/UserViewProducts'
+import ProductReviewChart from '../ProductReviewChart/ProductReviewChart'
 function ViewPosts() {
   return (
     <div className="view-post-container container-fluid">
       <div className="row view-post-row">
-        <div className="col-md-6">
-<h2>dssddsdsd</h2>
+        <div className="col-md-5 view-post-img-box">
+          <img src={defaultImg} alt="" />
         </div>
-        <div className="col-md-6">
-
+        <div className="col-md-6 view-post-name-box">
+          <h5>KEBILSHOP 3.5mm Stereo to Dual 6.3mm Mono Jack Audio Y Splitter Cable for Phone, Multimedia Speakers,Guitar, Sound Mixers, Other Professional Audio Equipment (4ft/1.2m, 1/PCS BLACK)</h5>
+          <p>product seller name</p>
+          <p>rating</p>
+          <p>number of users brought this item</p>
+          <h5><i className="fa-solid fa-indian-rupee-sign"></i> 25000</h5>
+          <p>if you order within 22hours the item will delivery on 20/10/2024</p>
         </div>
       </div>
       <div className="row view-post-row">
-        <div className="col-md-6">
+        <div className="col-md-5 view-post-other-box">
+          <div className="view-posts-action-box">
+            <button id='add-to-cart'>Add to Cart</button>
+            <button id='buy-now'>Buy now</button>
+            <button id='share'>Share <i className="fa-solid fa-share"></i></button>
+          </div>
+          <div className="view-post-other-box-inner">
+            <ProductReviewChart/>
+          </div>
 
         </div>
-        <div className="col-md-6">
-
+        <div className="col-md-6 view-post-description-box">
+          <p>About this item
+            Professional Series Stereo Plugs, connect instruments such as DJ System,synths, keyboards, guitars and other professional audio equipment.
+            Durable PVC jacket that protects the connectors from damage when bent slightly.
+            Cable is terminated using 3% silver solder,90% shield to remove any noise or humming.
+            Pure copper for optimum sound quality.Connectors: 3.5mm 1/8 Male x1; 6.35mm 1/4 Mono Jack Male x2;.Connecting your laptop or PC with a jack speaker output to a device with 2 x 6.35mm jack inputs - e.g. mixing desk.
+            1PCS Packing RED/BLUE Depend one Availability.</p>
+          <p>About this item
+            Professional Series Stereo Plugs, connect instruments such as DJ System,synths, keyboards, guitars and other professional audio equipment.
+            Durable PVC jacket that protects the connectors from damage when bent slightly.
+            Cable is terminated using 3% silver solder,90% shield to remove any noise or humming.
+            Pure copper for optimum sound quality.Connectors: 3.5mm 1/8 Male x1; 6.35mm 1/4 Mono Jack Male x2;.Connecting your laptop or PC with a jack speaker output to a device with 2 x 6.35mm jack inputs - e.g. mixing desk.
+            1PCS Packing RED/BLUE Depend one Availability.</p>
         </div>
       </div>
+      <h5 id='similar-products'>Similar products</h5>
+          <UserViewProducts />
     </div>
   )
 }
