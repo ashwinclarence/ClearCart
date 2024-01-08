90794fdd046a05454798f49403db7555c18ec120
ASHWIN CLARENCE <meashwinclarence@gmail.com>
2024-01-08 08:09:25 +0530

    user sign using signInWithEmailAndPassword

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -2,38 +2,59 @@ import { useState } from 'react'
 import loginLogo from '../../images/logomain.JPEG'
 import loginimg from '../../images/loginimg2.jpeg'
 import './Login.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../../Firebase/config'
 function Login() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const navigate = useNavigate()
+  // using signInWithEmailAndPassword to login existing user
+  const userLogin = (e) => {
+    e.preventDefault()
+    signInWithEmailAndPassword(auth, username, password).then((userCredential) => {
+      navigate('/')
+      alert("Welcome back " + userCredential.user.displayName);
+    }).catch((err) => {
+      setUsername("")
+      setPassword("")
+      document.getElementById("error-label").style.display = "block"
+      console.log(err.code);
+      console.log(err.message);
+    })
+  }
   return (
     <div className='container-fluid login-form-container'>
       <div className="row login-form">
         <div className="left-form">
-        <img src={loginimg} alt="" />
+          <img src={loginimg} alt="" />
         </div>
         <div className="right-form">
-        <form action="">
-          <img src={loginLogo} alt="" />
-          <input
-            type="email"
-            name='username'
-            placeholder='Username'
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required />
-          <input
-            type="password"
-            name='password'
-            placeholder='Password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required />
-          <button type='submit'>Login</button>
-        </form>
-       
-        <Link to='/register' className='back-home'><p>Don&#39;t have an account?</p></Link>
-        <Link to='/' className='back-home'><p>Go Back</p></Link>
+          <form action="" onSubmit={userLogin}>
+            <img src={loginLogo} alt="" />
+            <input
+              type="email"
+              name='username'
+              placeholder='Username'
+              value={username}
+              onChange={(e) =>{
+                setUsername(e.target.value)
+                document.getElementById("error-label").style.display="none"
+              } }
+              required />
+            <input
+              type="password"
+              name='password'
+              placeholder='Password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required />
+            <button type='submit'>Login</button>
+          </form>
+          <p id='error-label'>Invalid Username or Password</p>
+
+          <Link to='/register' className='back-home'><p>Don&#39;t have an account?</p></Link>
+          <Link to='/' className='back-home'><p>Go Back</p></Link>
         </div>
       </div>
     </div>
